4b8899de0132cd278ed78080d5aa7e8fd7d5ee76
ducduong <ducdv@vnpay.vn>
2023-02-17 13:45:29 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
-import React, { useEffect, useRef, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 function App() {
   const [data, setData] = useState([]);
 
@@ -11,48 +10,12 @@ function App() {
       crossorigin: true,
       mode: "no-cors",
     };
-    // fetch("http://anhchiem.xyz:43221/players.json", config)
-    //   .then((response) => response.json())
-    //   .then((buffer) => console.log(buffer))
-
-    //   .catch((error) => {
-    //     // console.error(error);
-    //   });
-
-    // const testURL = "http://anhchiem.xyz:43221/players.json";
-    // const myInit = {
-    //   method: "GET",
-    //   withCredentials: true,
-    //   crossorigin: true,
-    //   // mode: "no-cors",
-    //   mode: 'cors',
-    // };
-
-    // const myRequest = new Request(testURL, myInit);
-
-    // fetch(myRequest)
-    //   .then(function (response) {
-    //     console.log(response.type);
-    //     return response.text();
-    //   })
-    //   .then(function (response) {
-    //     console.log(response);
-    //   })
-    //   .catch(function (e) {
-    //     console.log(e);
-    //   });
-    var requestOptions = {
-      method: 'GET',
-
-      withCredentials: true,
-      crossorigin: true,
-
-    };
-    
-    fetch("http://anhchiem.xyz:43221/players.json", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log('result',result))
-      .catch(error => console.log('error', error));
+    fetch("http://anhchiem.xyz:43221/players.json", config)
+      .then((response) => response.text())
+      .then((buffer) => console.log(buffer))
+      .catch((error) => {
+        // console.error(error);
+      });
   }, []);
 
   return (
