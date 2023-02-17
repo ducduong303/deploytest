5f3cc24df58547bf9554a9da2d1720b76df5a57b
ducduong <ducdv@vnpay.vn>
2023-02-17 13:33:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const config = {
+      method: "GET",
+      withCredentials: true,
+      crossorigin: true,
+      mode: "no-cors",
+    };
+    // fetch("http://anhchiem.xyz:43221/players.json", config)
+    //   .then((response) => response.json())
+    //   .then((buffer) => console.log(buffer))
+
+    //   .catch((error) => {
+    //     // console.error(error);
+    //   });
+
+    // const testURL = "http://anhchiem.xyz:43221/players.json";
+    // const myInit = {
+    //   method: "GET",
+    //   withCredentials: true,
+    //   crossorigin: true,
+    //   // mode: "no-cors",
+    //   mode: 'cors',
+    // };
+
+    // const myRequest = new Request(testURL, myInit);
+
+    // fetch(myRequest)
+    //   .then(function (response) {
+    //     console.log(response.type);
+    //     return response.text();
+    //   })
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function (e) {
+    //     console.log(e);
+    //   });
+    var requestOptions = {
+      method: 'GET',
+
+      withCredentials: true,
+      crossorigin: true,
+
+    };
+    
+    fetch("http://anhchiem.xyz:43221/players.json", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log('result',result))
+      .catch(error => console.log('error', error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
